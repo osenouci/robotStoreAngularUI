8e06b64caf35af1f33219c40afa8c3add4bdd1f1
osenouci <=>
2017-11-14 10:41:24 +0100

    Added the login page design

---
+++ src/app/components/main-menu/main-menu.component.ts
@@ -0,0 +1,30 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-main-menu',
+  templateUrl: './main-menu.component.html',
+  styleUrls: ['./main-menu.component.scss'],
+  encapsulation: ViewEncapsulation.None
+})
+export class MainMenuComponent {
+
+  constructor(protected router:Router) { }
+
+
+  public navigateHome(){
+    this.router.navigate(['']);
+  }
+
+  public isLoggedIn() {
+    return false;
+  }
+  public login(){
+    this.router.navigate(['auth/login']);
+  }  
+  public logout(){
+    this.router.navigate(['auth/logout']);
+  }
+
+
+}
