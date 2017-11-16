59801256dc64f5c4ce0e42f3e5306a41848c3e19
osenouci <=>
2017-11-16 13:53:01 +0100

    Reworked the home page

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,17 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+
+let loadingTime = 2000;
 if (environment.production) {
   enableProdMode();
+  loadingTime = 0;
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+
+setTimeout(() => {
+
+  platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.log(err));
+
+}, loadingTime)
