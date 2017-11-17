2da8d5821e21391b9d2a3977ad239ec9d6c7a728
osenouci <=>
2017-11-17 12:59:49 +0100

    Added a photo gallery to display the photos

---
+++ src/app/modules/public/components/gallery/gallery.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, ViewEncapsulation, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-gallery',
+  templateUrl: './gallery.component.html',
+  styleUrls: ['./gallery.component.scss'],
+  encapsulation: ViewEncapsulation.None
+})
+export class GalleryComponent implements OnInit {
+
+  @Input() photos:Array<string> = new Array();
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/modules/public/components/product-details/product-details.component.ts
+++ src/app/modules/public/components/product-details/product-details.component.ts
@@ -1,16 +1,25 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
-  styleUrls: ['./product-details.component.css'],
+  styleUrls: ['./product-details.component.scss'],
   encapsulation: ViewEncapsulation.None
 })
 export class ProductDetailsComponent implements OnInit {
 
+  public galleryImages:Array<string> = new Array();
+
   constructor() { }
 
   ngOnInit() {
+    this.galleryImages.push('https://wallpaperscraft.com/image/milky_way_stars_ountains_night_germany_bavaria_sky_45888_1920x1080.jpg');
+    this.galleryImages.push('https://static.pexels.com/photos/36717/amazing-animal-beautiful-beautifull.jpg');
+    this.galleryImages.push('https://images7.alphacoders.com/411/thumb-1920-411820.jpg');
   }
+
+  protected configureGallery() {
+
+  }
+
 
 }
